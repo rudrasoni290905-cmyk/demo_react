cd00cf7ce5242dbbc2b890030aae6f74659c9d30
rudrasoni290905-cmyk <rudrasoni290905@gmail.com>
2026-04-14 07:32:43 +0000

    Day_6

--- my-firstproject/src/App.jsx
+++ my-firstproject/src/App.jsx
@@ -4,27 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  function button(){
-    alter ("You clicked on Button")
-  }
-  const fruit = ()=>{
-    alter ("Example of arrow function")
-  }
-  let data = "Demo"
-  function updata(){
-    data = "Example"
-    alter (data);
+  const [data,setdata] = useState("Hello");
+  function fun(){
+    setdata ("Changed value")  
   }
   return (
-    <div>
-      <h1>Example of Arrow function</h1>
-      <button onClick={button}>Click on the Button</button>
-      <button onClick={()=>fruit("First Parameter")}>Fruit Button</button>
-      <button onClick={()=>fruit("You clicked on Apple")}>Apple</button>
-      <button onClick={()=>fruit("You clicked on Banana")}>Banana</button>
-      <h2>{data}</h2>
-      <button onClick={updata}>Click to change value</button>
-    </div>
+    <>
+    <h1> State of variable </h1>
+    <h2> {data} </h2>
+    <button onClick={fun}>click</button>
+    </>
   )
 }
 export default App
